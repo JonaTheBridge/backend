971b3f09fb8440d3a1b2fc101b4dbbe18c85e19d
JonaTheBridge <jonathan@thebridgeschool.es>
2024-06-07 01:47:22 +0200

    update connection error management

--- 6.2) mongoose/database.js
+++ 6.2) mongoose/database.js
@@ -5,10 +5,11 @@ const MONGO_URL = 'mongodb+srv://admin:admin@cluster0.mpwutye.mongodb.net/';
 
 const connectionConfig = { dbName: MONGO_DB_NAME, autoIndex: true };
 mongoose.set('strictQuery', true);
-const connection = await mongoose.connect(MONGO_URL, connectionConfig);
 
-if (connection) {
+try {
+  await mongoose.connect(MONGO_URL, connectionConfig);
   console.log('Connection with mongo database successfully');
-} else {
+} catch (error) {
   console.error('Error to connect with mongo database');
+  console.error(error);
 }
